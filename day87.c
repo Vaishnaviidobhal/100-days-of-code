d4d8cd56efa0e35a6ad8980e5be4cf61beb7a688
Vaishnavi Dobhal <dobhalvaishnavii74@gmail.com>
2025-11-26 09:32:28 +0530

    Add role-based greeting program in C

---
+++ day87.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Role {
+    ADMIN,
+    USER,
+    GUEST
+};
+
+int main() {
+    char input[20];
+    enum Role r;
+
+    scanf("%s", input);
+
+    if (strcmp(input, "ADMIN") == 0)
+        r = ADMIN;
+    else if (strcmp(input, "USER") == 0)
+        r = USER;
+    else
+        r = GUEST;
+
+    switch (r) {
+        case ADMIN:
+            printf("Welcome Admin!");
+            break;
+        case USER:
+            printf("Welcome User!");
+            break;
+        case GUEST:
+            printf("Welcome Guest!");
+            break;
+    }
+
+    return 0;
+}

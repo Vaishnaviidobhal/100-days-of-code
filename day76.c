a85f575381e3dc60f2343b3c31ba3c41442188a0
Vaishnavi Dobhal <dobhalvaishnavii74@gmail.com>
2025-11-26 09:24:28 +0530

    Add file reading functionality in day76.c

---
+++ day76.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char filename[50];
+    char line[200];
+
+    printf("Enter filename: ");
+    scanf("%s", filename);
+
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error: File does not exist!");
+        return 1;
+    }
+
+    printf("File opened successfully.\n");
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+
